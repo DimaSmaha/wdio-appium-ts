cbbe4ec8d50736fcca64ac8f073237de557d21f0
DimaSmaha <dmytrosmaha@gmail.com>
2024-07-29 11:00:07 +0300

    upd tests

--- tests/specs/loginPage.spec.ts
+++ tests/specs/loginPage.spec.ts
@@ -1,11 +1,20 @@
 import loginPage from "../pageobjects/loginPage.page.js";
 
+//Should pass
 describe("Login/Sign Up Form", () => {
   it("should signup with valid credentials", async () => {
+    // await loginPage.signUp("testing123@mailnator.com", "SuperSecretPassword!");
+    // await expect(loginPage.popupSignupSuccessMessage).toBeExisting();
+    // await expect(loginPage.popupSignupSuccessMessage).toHaveTextContaining(
+    //   "You successfully signed up!",
+    // );
+
     await loginPage.signUp("testing123@mailnator.com", "SuperSecretPassword!");
-    await expect(loginPage.popupSignupSuccessMessage).toBeExisting();
-    await expect(loginPage.popupSignupSuccessMessage).toHaveTextContaining(
-      "You successfully signed up!",
-    );
+    await expect(
+      loginPage.isElementExist(loginPage.popupSignupSuccessMessage),
+    ).toBeExisting();
+    await expect(
+      loginPage.getElementText(loginPage.popupSignupSuccessMessage),
+    ).toHaveTextContaining("You successfully signed up!");
   });
 });
--- tests/specs/webviewPage.spec.ts
+++ tests/specs/webviewPage.spec.ts
@@ -1,9 +1,16 @@
 import webviewPage from "../pageobjects/webviewPage.page.js";
 
+//Should fail
 describe("Webview Page", () => {
   it("should verify webview page", async () => {
-    await webviewPage.webviewWidget.click();
-    await webviewPage.viewOnGH.scrollIntoView();
-    await expect(webviewPage.viewOnGH).toBeDisplayed();
+    // await $(webviewPage.webviewWidget).click();
+    // await $(webviewPage.viewOnGH).scrollIntoView();
+    // await expect($(webviewPage.viewOnGH)).toBeDisplayed();
+
+    await webviewPage.clickElement(webviewPage.webviewWidget);
+    await webviewPage.scrollElementIntoView(webviewPage.viewOnGH);
+    await expect(
+      webviewPage.isElementDisplayed(webviewPage.viewOnGH),
+    ).toBeDisplayed();
   });
 });
